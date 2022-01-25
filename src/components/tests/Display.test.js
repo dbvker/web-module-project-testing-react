91e6ed7b402aca4ea6d6bf47540a6437d83c363a
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-24 18:27:05 -0600

    push

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,11 +5,42 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+import fetchShow from '../../api/fetchShow';
+
+jest.mock('../../api/fetchShow');
+
+const testData = {
+    name: "show",
+    summary: "summary",
+    seasons: [
+        {
+            id: 0,
+            name: "season 1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "season 2",
+            episodes: []
+        }
+    ]
+};
 
 test('renders without errors with no props', ()=>{
-
+    render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async ()=>{
+    fetchShow.mockResolvedValueOnce(testData);
+    render(<Display />);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', ()=>{
+    render(<Display />);
+});
